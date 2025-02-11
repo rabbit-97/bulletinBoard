a2e2a298b8a044df1d4a946b4c0182f4a57f87f1
rabbit-97 <uzam97@gmail.com>
2025-02-11 12:19:40 +0900

    feat: 프로젝트 초기 설정
    - Express 서버 초기 설정
    - accountRouter 및 기본 라우트 설정
    - .env 및 .gitignore 파일 구성
    - Prisma 설정 및 데이터베이스 연결

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import accountRouter from './routes/accountRouter.js';
+
+const app = express();
+
+// 서버 시작
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.use('/api/account', accountRouter);
+
+
